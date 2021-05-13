395041a94f6784a5edbb7e0771d6a2ef843cf990
gittmp <tom.potter@durham.ac.uk>
2021-05-13 12:43:27 +0100

    correctly implemented X*Y rotating cubes

--- Wall.cs
+++ Wall.cs
@@ -5,19 +5,32 @@ using UnityEngine;
 public class Wall : MonoBehaviour
 {
     public Vector3 AnglePerSecond = new Vector3(0.0f, 180.0f, 0.0f);
-    private GameObject cube1;
 
-    void Start()
-    {
-        cube1 = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        cube1.transform.position = new Vector3(0.0f, 0.0f, 0.0f);
-        cube1.GetComponent<Renderer>().material.color = Color.white;
-        cube1.name = "Cube1";
+    public List<List<GameObject>> cubes = new List<List<GameObject>>();
+    public GameObject cube;
+    public int NoCubesX = 3;
+    public int NoCubesY = 3;
+
+    void Start(){
+        for(int x=0; x<NoCubesX; x++){
+            List<GameObject> cubesY = new List<GameObject>();
+            for(int y=0; y<NoCubesY; y++){
+                cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                cube.transform.position = new Vector3(0.0f + 2.0f * x, 0.0f + 2.0f * y, 0.0f);
+                cube.GetComponent<Renderer>().material.color = Color.white;
+                cube.name = "Cube-" + x.ToString() + '-' + y.ToString();
+                cubesY.Add(cube);
+            }
+            cubes.Add(cubesY);
+        }
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        cube1.transform.Rotate(AnglePerSecond * Time.deltaTime);
+    void Update(){
+        for(int x=0; x<NoCubesX; x++){
+            for(int y=0; y<NoCubesY; y++){
+                cubes[x][y].transform.Rotate(AnglePerSecond * Time.deltaTime);
+            }
+        }
     }
 }
